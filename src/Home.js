bd981143ad4f4b6c53faf94151f531bdb48f6170
Joaquín Arias <joaquinariasgomezcal@gmail.com>
2025-07-29 08:00:08 +0100

    Landing page taking shape

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './css/Home.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DashboardPage from './pages/DashboardPage';
+import { useCookie } from './useCookie';
+import LoginPage from './pages/LoginPage';
 
 /*
  * Home will be the page that holds the logic to decide if it renders "LoginPage", which is the frontpage
@@ -9,12 +10,28 @@ import DashboardPage from './pages/DashboardPage';
 */
 function Home() {
 
+  const [userJWTCookie, setUserJWTCookie, deleteUserJWTCookie] = useCookie("userJWT");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Context
+  //const [{userJWTCookie}] maybe I dont need to store the cookie in the context
+
+  useEffect(() => {
+    if (userJWTCookie !== "") {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [userJWTCookie]);
 
   const renderLoginPageOrDashboardPage = () => {
     if (isLoggedIn) {
       return (
         <DashboardPage />
+      )
+    } else {
+      return (
+        <LoginPage />
       )
     }
   }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,48 @@
-
+import { useEffect } from 'react';
+import '../css/LoginPage.css';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+
+  const authorization_url = process.env.REACT_APP_NOTION_AUTH_URL;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URL(window.document.location).searchParams;
+    const notionCode = params.get("code");
+    if (!notionCode) return;
+    navigate("/");
+    getLoginDataFromNotion(notionCode);
+  }, []);
+
+  const getLoginDataFromNotion = async (notionCode) => {
+    console.log("Getting logging data from Notion with this code ", notionCode);
+  }
+
   return (
-    <div>
-      This is the Login page
+    <div className="landingpage__backgroundwrapper">
+      <div className='landingpage__content'>
+        <div className='landingpage__body'>
+          <a className='landingpage__builtbyjoaquin__button' href='https://www.joaquinariasgomez.com/' target="_blank">
+            Built by Joaquín
+          </a>
+          <h1 className='landingpage__title'>
+            Turn your finance Notion databases
+            <br></br>
+            into insightful graphs
+          </h1>
+          <p className='landingpage__subtitle'>
+            Create automatically-updating graphs from your Notion account with no code.
+          </p>
+          <a className='landingpage__loginbutton' href={authorization_url}>
+            <p>Login with Notion</p>
+            <img src={process.env.PUBLIC_URL + '/notion_logo.png'} alt=''></img>
+          </a>
+        </div>
+        <div className='landingpage__footer'>
+          This is the footer
+        </div>
+      </div>
     </div>
   );
 }
