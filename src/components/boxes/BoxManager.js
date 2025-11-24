66c2c545277681d1ed5a78437ca79761a97e1d16
Joaquín Arias <joaquinariasgomezcal@gmail.com>
2025-11-24 14:09:15 +0000

    Lock scrolling if a box is opened

--- src/components/boxes/BoxManager.js
+++ src/components/boxes/BoxManager.js
@@ -26,6 +26,42 @@ export default function BoxManager() {
     };
   }, []);
 
+  // Lock body scroll when any box is open
+  useEffect(() => {
+    const isAnyBoxOpen = showSessionExpiredBox || showUserProfileBox || showNotionConnectionBox || showCreateGraphBox || showUpdateGraphConfigurationBox;
+
+    if (isAnyBoxOpen) {
+      // Save current scroll position
+      const scrollY = window.scrollY;
+
+      // Lock scroll
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = '100%';
+    } else {
+      // Restore scroll
+      const scrollY = document.body.style.top;
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+
+      // Restore scroll position
+      if (scrollY) {
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+      }
+    }
+
+    // Cleanup on unmount
+    return () => {
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+    };
+  }, [showSessionExpiredBox, showUserProfileBox, showNotionConnectionBox, showCreateGraphBox, showUpdateGraphConfigurationBox]);
+
   // TODO: if it is an alert box, render it before the other boxes
   const isAlertBox = () => {
 
